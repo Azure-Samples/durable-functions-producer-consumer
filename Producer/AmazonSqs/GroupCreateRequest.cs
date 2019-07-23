98849ca9e50b560e68e1b2a3469aa2dba27f93bb
Brandon🌩Hurlburt <hurlburb@microsoft.com>
2019-07-23 14:19:33 -0700

    initial commit

---
+++ Producer/AmazonSqs/GroupCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Producer.AmazonSqs
+{
+    internal class GroupCreateRequest
+    {
+        public string GroupId { get; set; }
+        public int NumberOfMessagesPerGroup { get; set; }
+        public string TestRunId { get; set; }
+        public int ConsumerWorkTime { get; set; }
+    }
+}
---
+++ Producer/AmazonSqs/GroupMessagesCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Producer.AmazonSqs
+{
+    internal class GroupMessagesCreateRequest
+    {
+        public string GroupId { get; set; }
+        public int MessageId { get; set; }
+        public DateTime EnqueueTimeUtc { get; set; }
+        public string TestRunId { get; set; }
+        public int ConsumerWorkTime { get; set; }
+    }
+}
