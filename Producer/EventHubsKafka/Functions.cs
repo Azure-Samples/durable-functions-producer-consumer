3b26f528920569569eea4dee53c7082c40849c30
Brandon H <hurlburb@microsoft.com>
2021-06-09 09:35:00 -0700

    [FIX] release-build compile error

--- Producer/EventHubsKafka/Functions.cs
+++ Producer/EventHubsKafka/Functions.cs
@@ -161,7 +161,7 @@ namespace Producer.EventHubsKafka
 #if DEBUG
                             log.LogTrace($@"Posted message {messageToPost.MessageId} (Size: {messageToPost.Message.Length} bytes) in {retryCount} attempt(s)");
 #else
-                log.LogTrace($@"Posted message for with TestRunID '{messageToPost.Properties[@"TestRunId"]}' and MessageID '{messageToPost.Properties[@"MessageId"]}' in {retryCount} attempt(s)");
+                            log.LogTrace($@"Posted message for with TestRunID '{messageToPost.TestRunId}' and MessageID '{messageToPost.MessageId}' in {retryCount} attempt(s)");
 #endif
                         }
                     } while (retry);
